5be3ec3549498eaf6645f38eb9cf54363bf8f756
Renato Suguiy <renatosuguiy@gmail.com>
2021-11-09 12:06:51 -0300

    feat(Dashboard): Create empty page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import {
   Center,
   Text,
   Heading,
+  Button,
   Stack,
   Skeleton,
 } from "@chakra-ui/react";
@@ -16,6 +17,8 @@ import { useTasks } from "../../contexts/TasksContext";
 import { useState, useEffect } from "react";
 import { ModalTaskDetail } from "../../components/Modal/ModalTaskDetail";
 import { CardSkeleton } from "../../components/Skeleton/CardSkeleton";
+import { ModalCreateTask } from "../../components/Modal/ModalCreateTask";
+import { FaClipboard } from "react-icons/fa";
 
 interface Task {
   id: string;
@@ -34,6 +37,12 @@ export const Dashboard = () => {
     isOpen: isTaskDetailOpen,
     onOpen: onTaskDetailOpen,
     onClose: onTaskDetailClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isCreateTaskOpen,
+    onOpen: onCreateTaskOpen,
+    onClose: onCreateTaskClose,
   } = useDisclosure();
 
   useEffect(() => {
@@ -112,25 +121,72 @@ export const Dashboard = () => {
         onClose={onTaskDetailClose}
         task={selectedTask}
       />
-      <Box>
-        <Header />
-        <SearchBox />
-        <Grid
-          w='100%'
-          templateColumns='repeat(auto-fill, minmax(420px, 1fr))'
-          gap={10}
-          paddingX='8'
-          mt='8'
-        >
-          {loading ? (
-            <CardSkeleton repeatCount={6} />
-          ) : (
-            tasks.map((task) => (
-              <Card key={task.id} task={task} onClick={handleClick} />
-            ))
-          )}
-        </Grid>
-      </Box>
+      {!loading && !tasks.length ? (
+        <>
+          <ModalCreateTask
+            isOpen={isCreateTaskOpen}
+            onClose={onCreateTaskClose}
+          />
+          <Heading>Nenhuma task</Heading>
+          <Header />
+          <Box
+            mt='4'
+            w='90vw'
+            paddingY='16'
+            paddingX={["6", "0"]}
+            ml='5vw'
+            justifyContent='center'
+            textAlign='center'
+            borderWidth='2px'
+            borderColor='gray.200'
+            borderStyle='dashed'
+          >
+            <Center fontSize='5xl'>
+              <FaClipboard color='#bdbdbd' />
+            </Center>
+            <Heading fontSize='4xl' as='h1' mt='4'>
+              Vamos criar sua primeira tarefa
+            </Heading>
+            <Text mt='6' color='gray.400'>
+              Insira sua meta e mostra a você mesmo <br />
+              capacidade em cumprir{" "}
+              <Text fontWeight='bold' color='gray.900'>
+                suas atividades
+              </Text>
+            </Text>
+            <Button
+              padding='6'
+              mt='6'
+              bgColor='purple.800'
+              color='white'
+              _hover={{ bg: "purple.900" }}
+              onClick={onCreateTaskOpen}
+            >
+              Criar sua primeira tarefa
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Box>
+          <Header />
+          <SearchBox />
+          <Grid
+            w='100%'
+            templateColumns='repeat(auto-fill, minmax(420px, 1fr))'
+            gap={10}
+            paddingX='8'
+            mt='8'
+          >
+            {loading ? (
+              <CardSkeleton repeatCount={6} />
+            ) : (
+              tasks.map((task) => (
+                <Card key={task.id} task={task} onClick={handleClick} />
+              ))
+            )}
+          </Grid>
+        </Box>
+      )}
     </>
   );
 };
